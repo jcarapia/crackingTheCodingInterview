66364b09bbd4ae75b9cc121ef35497c1289ae46e
Jorge <jcarapia83@Gmail.com>
2016-10-13 14:32:10 -0700

    solved 1.1

--- chapter01-arrays-and-strings/1.1-isUnique.js
+++ chapter01-arrays-and-strings/1.1-isUnique.js
@@ -5,5 +5,16 @@ var testString1 = 'abcde'; // true
 var testString2 = 'abcdec'; // false
 
 function unique(str){
-
+	for(var i = 0; i < str.length; i++){
+		//you only need to search the section of the string after the ith element
+		for(var j = i+1; j < str.length; j++){
+			if(str[i] === str[j]){
+				return false;
+			}
+		}
+	}
+	return true;
 };
+
+console.log(unique(testString1)); //true
+console.log(unique(testString2)); //false
