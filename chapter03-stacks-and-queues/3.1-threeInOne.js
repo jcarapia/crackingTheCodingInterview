63c64bdf1530c2714bbc3a94230fbd7fb2d8803e
Jorge <jcarapia83@Gmail.com>
2016-11-01 18:09:11 -0700

    solved threeInOne

---
+++ chapter03-stacks-and-queues/3.1-threeInOne.js
@@ -0,0 +1,9 @@
+/*
+Three in One
+Describe how you could use a single array to implement 
+three stacks.
+
+1. This problem is more suited to Java than JS. 
+ 		In Java, the length of an array is established when it 
+ 		is created and it is fixed. In JS, the length is flexible.
+*/
