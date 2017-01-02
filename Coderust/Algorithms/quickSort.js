eb88a64c728e2117696b59b05d598610dcd813e3
Jorge <jcarapia83@Gmail.com>
2017-01-01 21:43:24 -0800

    working on quicksort

---
+++ Coderust/Algorithms/quickSort.js
@@ -0,0 +1,11 @@
+/*
+Quicksort
+
+Given an integer array, sort it in ascending order using quicksort. 
+
+Original array: [55, 23, 26, 2, 25];
+Sorted array: [2, 23, 25, 26, 55];
+
+Time complexity: n*log(n);
+
+*/
