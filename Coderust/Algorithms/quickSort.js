b7bafc0aec7a1edbe17e2c77e2ba92609c05333f
Jorge <jcarapia83@Gmail.com>
2017-01-03 17:20:57 -0800

    working on quicksort partition function

--- Coderust/Algorithms/quickSort.js
+++ Coderust/Algorithms/quickSort.js
@@ -10,6 +10,30 @@ Time complexity: n*log(n);
 
 */
 
-function quickSort(array, low, high) {
+function quickSort(array) {
+	quickSortRecurse(array, array[0], array.length-1);
+};
 
+function quickSortRecurse(array, left, right) {
+	if(left >= right) {
+		return;
+	}
+	var pivotIndex = partition(array, left, right);
+	quickSortRecurse(array, left, index-1);
+	quickSortRecurse(array, index+1, right); 
+};
+
+function partition(array, left, right) {
+	var pivot = array[left];
+	var i = left; 
+	var j = right; 
+
+	while(i < j) {
+
+	}
 }
+
+quickSort([1,3,2,5,4,7,6])
+
+
+
