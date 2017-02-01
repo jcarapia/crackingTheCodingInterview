bc9b0b8e26aa027379fd7149cd35b4e9055cca6b
Jorge <jcarapia83@Gmail.com>
2017-02-01 15:14:20 -0800

    solved funnyString

--- Misc/funnyString.js
+++ Misc/funnyString.js
@@ -1,1 +1,28 @@
-console.log('abc'.split("").reverse().join(""))
+function processData(input) {
+    //Enter your code here
+    var input_array = input.split("\n");
+    for(var j = 1; j < input_array.length; j++) {
+      console.log(funny(input_array[j]));
+    }
+};
+
+function funny(string) {
+  var s_0, s_1, r_0, r_1, s_diff, r_diff;
+  
+  var rev_string = string.split("").reverse().join("");
+  
+  for(var i = 1; i < string.length; i++){
+    s_0 = string.charCodeAt(i-1);
+    s_1 = string.charCodeAt(i);
+    r_0 = rev_string.charCodeAt(i-1);
+    r_1 = rev_string.charCodeAt(i);
+    
+    s_diff = Math.abs(s_1 - s_0);
+    r_diff = Math.abs(r_1 - r_0);
+    
+    if(s_diff !== r_diff) {
+      return "Not Funny";
+    };
+  };
+  return "Funny";
+};
