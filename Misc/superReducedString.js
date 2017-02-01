34194462c215d454a7c9eacd949261835025b308
Jorge <jcarapia83@Gmail.com>
2017-01-31 23:56:34 -0800

    solved super reduced string

---
+++ Misc/superReducedString.js
@@ -0,0 +1,44 @@
+
+/*
+Super Reduced String
+
+Given a string of lower case letters, reduce the string via the following rules: 
+1) If the string has 2 identical consecutive letters, remove those two letters
+2) Remove one set of duplicate letters per pass of the loop
+3) If the string is completely deleted, print "EMPTY STRING"
+4) Otherwise print the super reduced string
+
+example 1: 
+input = 'aabbcc'
+output = 'EMPTY STRING'
+
+example 2: 
+input = 'aaabbbccc'
+output = 'abc'
+
+*/
+
+var input = 'aabbbccddd';
+
+function superReducedString(string) {
+	for(var i = 0; i < string.length; i++) {
+		if(string[i] === string[i+1]) {
+			string = string.slice(0,i) + string.slice(i+2) //remove ith plus i+1th
+			i = i - 1; // take into account that an element was removed so do not advance or an element will
+								 // be skipped
+		}
+	};
+
+	if(string.length === 0) {
+		console.log('EMPTY STRING');
+	};
+
+	console.log(string);
+}
+				// 0 to element before 2 + third to last
+ // input = input.slice(0,2) + input.slice(3)
+ // console.log(input)
+
+superReducedString(input)
+
+
