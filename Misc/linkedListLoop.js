4a505846e1d282b135fa9691735a39a5ece0c202
Jorge <jcarapia83@Gmail.com>
2017-02-10 14:15:34 -0800

    solved linkedListLoop

---
+++ Misc/linkedListLoop.js
@@ -0,0 +1,89 @@
+/*
+ * Assignment: Write a function that returns true if a linked list contains a cycle, or false if it terminates somewhere
+ *
+ * Explanation:
+ * 
+ * Generally, we assume that a linked list will terminate in a null next pointer, as follows:
+ *
+ * A -> B -> C -> D -> E -> null
+ *
+ * A 'cycle' in a linked list is when traversing the list would result in visiting the same nodes over and over
+ * This is caused by pointing a node in the list to another node that already appeared earlier in the list. Example:
+ *
+ * A -> B -> C
+ *      ^    |
+ *      |    v
+ *      E <- D
+ *
+ * Example code:
+ *
+ * var nodeA = Node('A');
+ * var nodeB = nodeA.next = Node('B');
+ * var nodeC = nodeB.next = Node('C');
+ * var nodeD = nodeC.next = Node('D');
+ * var nodeE = nodeD.next = Node('E');
+ * hasCycle(nodeA); // => false
+ * nodeE.next = nodeB;
+ * hasCycle(nodeA); // => true
+ *
+ * Constraint 1: Do this in linear time
+ * Constraint 2: Do this in constant space
+ * Constraint 3: Do not mutate the original nodes in any way
+ */
+
+ //Cannot check for absence of null, as it may result in an infinite loop
+ //
+
+var Node = function(value){
+  return { value: value, next: null };
+};
+
+var nodeA = Node('A');
+var nodeB = nodeA.next = Node('B');
+var nodeC = nodeB.next = Node('C');
+var nodeD = nodeC.next = Node('D');
+var nodeE = nodeD.next = Node('E'); 
+nodeE.next = nodeB;
+
+//console.log(nodeA)
+
+// Use the tortoise and the hare algorithm in which one pointer goes faster than the other 
+// if they meet, there is a loop. 
+
+
+
+function linkedListLoop(linkedList) {
+
+	var tortoise = linkedList;
+	var hare = linkedList.next;
+
+	while(tortoise && hare) {
+
+		if(tortoise.value === hare.value) {
+			return true
+		};
+
+		tortoise = tortoise.next;
+		hare = hare.next.next;
+
+	}
+	return false
+};
+
+console.log(linkedListLoop(nodeA))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
