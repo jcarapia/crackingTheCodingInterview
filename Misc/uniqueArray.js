61fe2177afc6d5f891581dd5755f5097c4c75ece
Jorge <jcarapia83@Gmail.com>
2017-02-12 14:12:37 -0800

    solved uniqueArray

---
+++ Misc/uniqueArray.js
@@ -0,0 +1,22 @@
+/*
+Given an array, return an array without duplicates
+
+*/
+
+function uniqueArray(array) {
+	var container = {};
+	var uniqueArray = [];
+
+	for(var i = 0; i < array.length; i++){
+		if(!container[array[i]]){
+			container[array[i]] = 1;
+			uniqueArray.push(array[i]);
+
+		}
+	}
+	//return Object.keys(container);
+	return uniqueArray; //this method maintains the type of element in the original array
+											//Using Object.keys returns an array of string. 
+};
+
+console.log(uniqueArray([1,1,2,2,3,4,5,5,6,6,6,7,8,9]))
